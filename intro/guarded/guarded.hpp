6488754d168c8245ece8e4d7d839f97a5d454ed9
Danila Mishin <>
2021-02-07 15:45:15 +0300

    first try

---
+++ intro/guarded/guarded.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <twist/stdlike/mutex.hpp>
+
+namespace solutions {
+
+// Automagically wraps all accesses to guarded object to critical sections
+// Look at unit tests for API and usage examples
+template <typename T>
+class Guarded {
+ public:
+  // Custom reference class. Allows us to unlock mutex as long as
+  // the resource is not used (as long as pointer is getting destroyed).
+  class GuardedReference {
+   private:
+    T* object_;
+    twist::stdlike::mutex& mutex_;
+
+   public:
+    explicit GuardedReference(T* object_ref, twist::stdlike::mutex& mutex)
+        : object_(object_ref), mutex_(mutex) {
+      mutex_.lock();
+    }
+
+    ~GuardedReference() {
+      mutex_.unlock();
+    }
+
+    T* operator->() {
+      return object_;
+    }
+  };
+
+  // https://eli.thegreenplace.net/2014/perfect-forwarding-and-universal-references-in-c/
+  template <typename... Args>
+  Guarded(Args&&... args) : object_(std::forward<Args>(args)...) {
+  }
+
+  GuardedReference operator->() {
+    return GuardedReference(&object_, mutex_);
+  }
+  // https://en.cppreference.com/w/cpp/language/operators
+
+ private:
+  T object_;
+  twist::stdlike::mutex mutex_;  // Guards access to object_
+};
+
+}  // namespace solutions
