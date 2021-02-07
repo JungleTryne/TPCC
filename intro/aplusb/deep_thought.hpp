eb3e6dc4ac258e5bf2e67c0be080dcb094e3a80e
Danila Mishin <>
2021-02-07 12:56:00 +0300

    fix bug

--- intro/aplusb/deep_thought.hpp
+++ intro/aplusb/deep_thought.hpp
@@ -6,7 +6,7 @@ class DeepThought {
  public:
   // Computes the answer to life, the universe and everything
   int ComputeAnswer() {
-    return 42;  // Your code goes here
+    return 42;
   }
 };
 
