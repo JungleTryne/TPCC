7338033b27b9c4a6ca3858c8ef1ce788bf8abd2f
Danila Mishin <>
2021-03-04 23:50:47 +0300

    Bump task condvar/semaphore

---
+++ condvar/semaphore/blocking_queue.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "semaphore.hpp"
+
+#include <deque>
+
+namespace solutions {
+
+// Bounded Blocking Multi-Producer/Multi-Consumer (MPMC) Queue
+
+template <typename T>
+class BlockingQueue {
+ public:
+  explicit BlockingQueue(size_t capacity)
+      : bound_top_tokens_(capacity),
+        bound_bottom_tokens_(0),
+        queue_protector_(1)  // Only one can use the container
+  {
+  }
+
+  // Inserts the specified element into this queue
+  void Put(T value) {
+    bound_top_tokens_.Acquire();
+
+    queue_protector_.Acquire();
+    buffer_.push_back(std::move(value));
+    queue_protector_.Release();
+
+    bound_bottom_tokens_.Release();
+  }
+
+  // Retrieves and removes the head of this queue,
+  // waiting if necessary until an element becomes available
+  T Take() {
+    bound_bottom_tokens_.Acquire();
+
+    queue_protector_.Acquire();
+    T value = std::move(buffer_.front());
+    buffer_.pop_front();
+    queue_protector_.Release();
+
+    bound_top_tokens_.Release();
+    return value;
+  }
+
+ private:
+  // Implements top boundedness, i.e. buffer.size() <= capacity
+  Semaphore bound_top_tokens_;
+
+  // Implements bottom boundedness, i.e. buffer.size() >= 0
+  Semaphore bound_bottom_tokens_;
+
+  // Mutex substitute
+  Semaphore queue_protector_;
+
+  // Buffer
+  std::deque<T> buffer_;
+};
+
+}  // namespace solutions
